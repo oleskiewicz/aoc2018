0dfc62ef708ad202095520949fad9108d7ac2399
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2018-12-07 09:44:17 +0000

    Day 7, part 1
    Closer, but now not printing final element at all.  Graph is not as eager now,
    still struggling with "availability" first criterion.

--- src/07.jl
+++ src/07.jl
@@ -1,25 +1,32 @@
 #!/usr/bin/env dk 202a3fcc9519 julia
 using DelimitedFiles
 
-function find_first(table::Array{Char,2})::Array{Char}
+function find_prerequisites(table::Array{Char,2})::Array{Char}
   setdiff(Set(table[:,1]), Set(table[:,2])) |> collect |> sort
 end
 
-function flatten_graph(firsts, table)
-  sort!(firsts)
-  for c in firsts
-    println(c)
-  end
-  nexts::Array{Char,1} = []
-  for c in firsts
-    append!(nexts, table[table[:,1] .== c, 2])
-    flatten_graph(
-      table[table[:,1] .== c, 2],
-      table[table[:,1] .!= c, :]
-    )
-  end
+function flatten_graph(table)
+	if ! isempty(table)
+		prerequisites = find_prerequisites(table)
+		for c in prerequisites
+			println(c)
+		end
+		flatten_graph(table[findall(c-> !(c in prerequisites), table[:,1]),:])
+	else
+		return
+	end
+
+	# nexts::Array{Char,1} = []
+	# for c in prerequisites
+	#   append!(nexts, table[table[:,1] .== c, 2])
+	#   flatten_graph(
+	#     table[table[:,1] .== c, 2],
+	#     table[table[:,1] .!= c, :]
+	#   )
+	# end
+
 end
 
 table = readdlm("./dat/07_test.csv", ',', Char, '\n')
-beginning = find_first(table)
-flatten_graph(beginning, table)
+beginning = find_prerequisites(table)
+flatten_graph(table)
