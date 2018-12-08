b9a860f2d0559530df2456993efad3984b04fbd4
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2018-12-08 22:59:07 +0000

    Day 8, part 1

---
+++ src/p08.nim
@@ -0,0 +1,34 @@
+import sequtils, strutils, sugar, os, math
+
+type Tree = object
+  children: seq[Tree]
+  metadata: seq[int]
+
+proc read(filename: string): seq[int] =
+  filename.readFile.strip.split(" ").map(x => parseInt(x))
+
+func build(data: seq[int]): (Tree, seq[int]) =
+  var
+    num_children = data[0]
+    num_metadata = data[1]
+    new_data = data[2..^1]
+    child: Tree
+    children: seq[Tree] = @[]
+    metadata: seq[int] = @[]
+
+  for _ in 0..<num_children:
+    (child, new_data) = build(new_data)
+    children &= child
+
+  metadata = new_data[0..<num_metadata]
+  new_data = new_data[num_metadata..^1]
+
+  return (Tree(children: children, metadata: metadata), new_data)
+
+func sum(tree: Tree): int =
+  tree.metadata.sum + tree.children.map(child => sum(child)).sum
+
+when isMainModule:
+  var (tree, _) = paramStr(1).read.build
+  echo tree.sum
+
