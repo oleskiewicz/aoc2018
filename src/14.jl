443b2ca2d60075ca16eb74f260dcdb98b949e681
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2018-12-14 15:37:06 +0100

    Day 14, part 2 (no star)
    confused as to what part 2 is asking

--- src/14.jl
+++ src/14.jl
@@ -17,10 +17,34 @@ function improve(scores::Array{Int,1}, e1::Int, e2::Int, iters::Int)::String
 	return scores[iters+1:iters+10] |> join
 end
 
+function backwards(scores::Array{Int,1}, e1::Int, e2::Int, a1::String)::Int
+	iters::Int = 0
+	while true
+		scores, e1, e2 = tick(scores, e1, e2)
+		if length(scores) < length(a1)
+			continue
+		elseif join(scores[iters+1:iters+length(a1)]) == a1
+			break
+		end
+		iters += 1
+	end
+	return iters
+end
+
 # Part 1
 @test improve([3, 7], 1, 2, 9) == "5158916779"
 @test improve([3, 7], 1, 2, 5) == "0124515891"
 @test improve([3, 7], 1, 2, 18) == "9251071085"
 @test improve([3, 7], 1, 2, 2018) == "5941429882"
 
-println(improve([3, 7], 1, 2, 920831))
+a1 = improve([3, 7], 1, 2, 920831)
+println(a1)
+
+# Part 2
+@test backwards([3, 7], 1, 2, improve([3, 7], 1, 2, 9)[1:5]) == 9
+@test backwards([3, 7], 1, 2, "01245") == 5
+@test backwards([3, 7], 1, 2, "92510") == 18
+@test backwards([3, 7], 1, 2, "59414") == 2018
+
+a2 = backwards([3, 7], 1, 2, a1[1:10])
+println(a2)
