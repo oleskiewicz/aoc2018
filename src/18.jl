6081b9bc1e7d9990a975f8b54589857d2b880280
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2018-12-18 20:02:05 +0100

    Day 18, part 1 finished
    - remember to copy arrays (every minute update is global)
    - hard-coded array size (otherwise Julia's string splitting is bizzare)
    - grid printing utility

--- src/18.jl
+++ src/18.jl
@@ -2,25 +2,23 @@
 using Test
 
 function read_grid(filename)
-  grid = Matrix{Char}(undef, 10, 10)
+  grid = Matrix{Char}(undef, 50, 50)
   for (y, line) in enumerate(readlines(filename))
     for (x, char) in enumerate(split(line, ""))
     grid[y,x] = Char(char[1])
     end
   end
-  #= split.(readlines(filename),"") =#
-  grid
+  return grid
 end
 
 function neighbours(grid, x, y)
   x_min = x - 1 < 1 ? 1 : x - 1
-  x_max = x + 1 > 10 ? 10 : x + 1
+  x_max = x + 1 > 50 ? 50 : x + 1
   y_min = y - 1 < 1 ? 1 : y - 1
-  y_max = y + 1 > 10 ? 10 : y + 1
+  y_max = y + 1 > 50 ? 50 : y + 1
   result = copy(grid)
   result[x,y] = ' '
-  result = result[x_min:x_max,y_min:y_max]
-  result
+  return result[x_min:x_max,y_min:y_max]
 end
 
 function rules(cell, neighbourhood)
@@ -28,21 +26,25 @@ function rules(cell, neighbourhood)
   n_trees = count(c -> c == '|', neighbourhood)
   n_lumber = count(c -> c == '#', neighbourhood)
 
-  # An open acre will become filled with trees if three or more adjacent acres contained trees. Otherwise, nothing happens.
+  # An open acre will become filled with trees if three or more adjacent acres
+  # contained trees. Otherwise, nothing happens.
   if cell == '.'
     if n_trees >= 3
       return '|'
     else
       return '.'
     end
-  # An acre filled with trees will become a lumberyard if three or more adjacent acres were lumberyards. Otherwise, nothing happens.
+  # An acre filled with trees will become a lumberyard if three or more
+  # adjacent acres were lumberyards. Otherwise, nothing happens.
   elseif cell == '|'
     if n_lumber >= 3
       return '#'
     else
       return '|'
     end
-  # An acre containing a lumberyard will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees. Otherwise, it becomes open.
+  # An acre containing a lumberyard will remain a lumberyard if it was
+  # adjacent to at least one other lumberyard and at least one acre containing
+  # trees. Otherwise, it becomes open.
   elseif cell == '#'
     if n_lumber >= 1 && n_trees >= 1
       return '#'
@@ -52,15 +54,40 @@ function rules(cell, neighbourhood)
   end
 end
 
-function advance!(grid)
-  for x in 1:10
-    for y in 1:10
-      grid[x,y] = rules(grid[x,y], neighbours(grid,x,y))
+function advance!(grid::Array{Char,2})::Array{Char,2}
+  next_grid = copy(grid)
+  for x in 1:50
+    for y in 1:50
+      next_grid[x,y] = rules(grid[x,y], neighbours(grid, x, y))
     end
+  end
+  return next_grid
+end
+
+function advance!(grid::Array{Char,2}, n_steps::Int)::Array{Char,2}
+  for t in 1:n_steps
+    grid = advance!(grid)
+  end
+  return grid
+end
+
+function value(grid)
+  return count(c -> c == '#', grid) * count(c -> c == '|', grid)
+end
+
+function p(grid)
+  for x in 1:50
+    for y in 1:50
+      print(grid[x,y])
+    end
+    print("\n")
   end
 end
 
 # Part 1
-grid = read_grid("./dat/18_test.txt")
-println(grid)
+#= @test advance!(read_grid("./dat/18_test.txt"), 10) |> value == 1147 =#
+read_grid("./dat/18.txt") |>
+  (x -> advance!(x, 10)) |>
+  value |>
+  println
 
